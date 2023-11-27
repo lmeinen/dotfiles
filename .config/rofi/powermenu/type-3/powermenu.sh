15d43f80a6c608ce5666b2cc940d29d64c698505
Lasse_Meinen <lasse.meinen@gmail.com>
2023-11-27 09:12:42 +0100

    updated powermenu

--- .config/rofi/powermenu/type-3/powermenu.sh
+++ .config/rofi/powermenu/type-3/powermenu.sh
@@ -61,19 +61,13 @@ run_cmd() {
 		elif [[ $1 == '--reboot' ]]; then
 			systemctl reboot
 		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
+			playerctl -a pause
+			pactl set-sink-mute @DEFAULT_SINK@ 1
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+			loginctl terminate-session self
+		elif [[ $1 == '--lock' ]]; then
+			swaymsg "exec $HOME/.config/sway/lock.sh -i"
 		fi
 	else
 		exit 0
@@ -90,11 +84,7 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
+		run_cmd --lock
         ;;
     $suspend)
 		run_cmd --suspend
